fab1ab54b5e7c0b25b5c52a8462899048510ffe5
Samil ERDOGAN <samil.erdogan@outlook.com>
2020-06-09 14:59:24 +0200

    addin first controller

---
+++ src/Controller/Main/MainController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Main;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(Request $request)
+    {
+        $locale = $request->getLocale();
+        return $this->render('main/index.html.twig');
+    }
+
+    /**
+     * @Route("/privacy", name="privacy_policy")
+     */
+    public function privacy(){
+        return $this->render('legal/privacy.html.twig');
+    }
+
+
+}
