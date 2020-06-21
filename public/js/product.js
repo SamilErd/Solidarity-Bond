6939a43fdde62ffd0c3fa3a1a4b64415507dd043
Samil ERDOGAN <samil.erdogan@outlook.com>
2020-06-21 21:56:51 +0200

    Admin Product Page push

---
+++ public/js/product.js
@@ -0,0 +1,52 @@
+function ShowName() {
+    var y = document.getElementById("N");
+    var x = document.getElementById("NI");
+    if (x.style.display === "none") {
+        x.style.display = "block";  
+        y.style.display = "none";  
+    } else {
+        y.style.display = "block";
+        x.style.display = "none";  
+    } 
+ }      
+function ShowPrice() {
+    var y = document.getElementById("P");
+    var x = document.getElementById("PI");
+    if (x.style.display === "none") {
+        x.style.display = "block";  
+        y.style.display = "none";  
+    } else {
+        y.style.display = "block";
+        x.style.display = "none";  
+    } 
+ }
+function ShowStock() {
+    var y = document.getElementById("S");
+    var x = document.getElementById("SI");
+    if (x.style.display === "none") {
+        x.style.display = "block";  
+        y.style.display = "none";  
+    } else {
+        y.style.display = "block";
+        x.style.display = "none";  
+    } 
+ }
+ function ShowDescription() {
+    var y = document.getElementById("D");
+    var x = document.getElementById("DI");
+    if (x.style.display === "none") {
+        x.style.display = "block";  
+        y.style.display = "none";  
+    } else {
+        y.style.display = "block";
+        x.style.display = "none";  
+    } 
+ }
+ function ShowImage() {
+    var x = document.getElementById("II");
+    if (x.style.display === "none") {
+        x.style.display = "block";  
+    } else {
+        x.style.display = "none";  
+    } 
+ }
--- src/Controller/Shop/ShopController.php
+++ src/Controller/Shop/ShopController.php
@@ -40,8 +40,68 @@ class ShopController extends AbstractController
      */
     public function show_product($id, ProductRepository $prepo)
     {
+
+        
         //finding the specific product in the database
         $product = $prepo->find($id);
+        
+        //Getting an instance of the entity manager
+        $entityManager = $this->getDoctrine()->getManager();
+        if(!empty($_POST["NI"])){
+            $product->setProductName($_POST['NI']);
+            $entityManager->persist($product);
+            //basically updating the user infos in the database
+            $entityManager->flush();
+            return $this->redirectToRoute('show_product', ['id' => $id]);
+        }
+        if(!empty($_POST["PI"])){
+            $product->setPrice($_POST['PI']);
+            $entityManager->persist($product);
+            //basically updating the user infos in the database
+            $entityManager->flush();
+            return $this->redirectToRoute('show_product', ['id' => $id]);
+        }
+        if(!empty($_POST["SI"])){
+            $product->setStock($_POST['SI']);
+            $entityManager->persist($product);
+            //basically updating the user infos in the database
+            $entityManager->flush();
+            return $this->redirectToRoute('show_product', ['id' => $id]);
+        }
+        if(!empty($_POST["DI"])){
+            $product->setDescription($_POST['DI']);
+            $entityManager->persist($product);
+            //basically updating the user infos in the database
+            $entityManager->flush();
+            return $this->redirectToRoute('show_product', ['id' => $id]);
+        }
+
+        
+         /*   if(!empty($_POST["DI"])){
+                $target_dir = "uploads/";
+                $file = $target_dir . basename($_FILES["II"]["name"]);
+                
+                $uploadOk = 1;
+                $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+                $fileName = md5(uniqid()).'.'.$imageFileType;
+                // Check if image file is a actual image or fake image
+                if(isset($_POST["submit"])) {
+                $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+                if($check !== false) {
+                    dd( "File is an image - " . $check["mime"] . ".");
+                    $uploadOk = 1;
+                } else {
+                    dd("File is not an image.");
+                    $uploadOk = 0;
+                }
+            }
+        }*/
+
+
+
+
+
         //rendering the specific product's page
         return $this->render('shop/product.html.twig', [
             "product" => $product,
